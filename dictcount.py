7f2b0f59f932041779636a75c4581d2e786b1050
sunshine-7 <1194828958@qq.com>
2019-09-04 20:45:13 +0800

    calculate the number of each element in the file

--- dictcount.py
+++ dictcount.py
@@ -1,13 +1,22 @@
 #计算文件中每个元素的个数
+# f=open("raw.txt")
+# col=[]
+# dict = {}
+# list=[line.strip() for line in f]
+# for i in list:
+#     i=i.split(",")
+#     col+=i
+# print(col)
+# for j in col:
+#     dict[j] = dict.get(j, 0) + 1
+# print(dict)
+# f.close()
+#简洁版
+#计算文件中每个元素的个数
+d = {}
 f=open("raw.txt")
-col=[]
-dict = {}
-list=[line.strip() for line in f]
-for i in list:
-    i=i.split(",")
-    col+=i
-print(col)
-for j in col:
-    dict[j] = dict.get(j, 0) + 1
-print(dict)
-f.close()
+with open("raw.txt", "r") as f:
+    for line in f:
+         for j in line.strip().split(","):
+            d[j] = d.get(j, 0) + 1
+print(d)
