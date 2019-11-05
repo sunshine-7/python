3ea92130a94037f7f81c8fda822f1684145c6cb7
sunshine-7 <1194828958@qq.com>
2019-11-05 21:26:08 +0800

    update first

--- data_structure/sqlist.py
+++ data_structure/sqlist.py
@@ -1,3 +1,4 @@
+# 函数的头字母用小写
 class Node(object):
     """节点"""
     def __init__(self, data):
@@ -38,53 +39,59 @@ class SQList(object):
             p.next = node
             self.count += 1
 
-    def ListLength(self):
+    def listLength(self):
         """输出单链表的长度"""
-        n=0
-        p=self.head
-        while p is not None:
-            n+=1
-            p=p.next
-        print("链表长度为："+str(n))
+        # n=0
+        # p=self.head
+        # while p is not None:
+        #     n+=1
+        #     p=p.next
+        # print("链表长度为："+str(n))
+        # 最好有返回值，一般只需知道返回值，不用打印，
+        # 直接用 return self.count
+        return self.count
 
-    def ListEmpty(self):
+    def listEmpty(self):
         """判断链表是否为空"""
         p=self.head
         if p is None:
-            print("链表为空")
+            return True
         else:
-            print("链表不为空")
+            return False
 
-    def printspe(self):
+    def printSpe(self):
         """输出某个特定位置的元素"""
         p=self.head
         j=0
-        i=3
+        i=int(input("要查询哪个位置的元素？"))
         while(j<i-1 and p is not None):
             p=p.next
             j+=1
         if p is None:
             print("元素不存在")
         else:
-            print("第三个元素为："+str(p.data))
+            print("第%d个元素为：" %i +str(p.data))
 
-    def printpos(self):
+    def printPos(self):
         """输出某个特定元素的位置"""
         p=self.head
         i=1
+        num=int(input("要查询哪个元素的位置？"))
         while(p is not None):
-            if p.data==2:
-                print("元素2的位置为："+str(i))
+            if p.data==num:
+                print("元素%d的位置为：" %num +str(i))
                 break
             p=p.next
             i+=1
         if p is None:
             print("没有找到此元素")
 
-    def insertspe(self,node):
+
+    # 在第一个位置插入元素有些问题，再改改！！！！！！！！！！！！！！
+    def insertSpe(self,node):
         """在某个特定的位置插入某个元素"""
         p=self.head
-        i=3
+        i=int(input("要在哪个位置插入元素？\n"))
         j=0
         while(j<i-2 and p is not None):
             p=p.next
@@ -96,11 +103,12 @@ class SQList(object):
             p.next=node
             p=p.next
             p.next=t
-            print("在第3个元素位置上插入元素5：")
+            self.count+=1
+            # print("在第%d个元素位置上插入元素%d：" %i %(node.data))
 
-    def deletespe(self):
+    def deleteSpe(self):
         """删除某个特定位置的元素"""
-        i=3
+        i=int(input("要删除哪个位置的元素？\n"))
         j=0
         p=self.head
         while(j<i-2 and p is not None):
@@ -111,7 +119,7 @@ class SQList(object):
         else:
             t=p.next
             p.next=t.next
-            print("删除第三个元素：")
+            print("删除第%d个元素：" %i)
             self.count-=1
 
 
@@ -126,7 +134,7 @@ class SQList(object):
 
     def delete(self):
         """释放单链表"""
-        print("删除链表")
+        print("释放单链表")
         self.head=None
         self.count=0
 
@@ -134,22 +142,29 @@ class SQList(object):
 
 if __name__ == '__main__':
     sqlist = SQList()
-    node = Node(2)
-    node1 = Node(3)
+    n=int(input("请输入要插入的数据：\n"))
+    node = Node(n)
+    n1 = int(input("请输入要插入的数据：\n"))
+    node1 = Node(n1)
     # sqlist.print()
     sqlist.append(node)
     sqlist.append(node1)
-    node2 = Node(4)
+    n2 = int(input("请输入要插入的数据：\n"))
+    node2 = Node(n2)
     sqlist.headInsert(node2)
     sqlist.print()
-    sqlist.ListLength()
-    sqlist.ListEmpty()
-    sqlist.printspe()
-    sqlist.printpos()
-    node3=Node(5)
-    sqlist.insertspe(node3)
+    print("链表长度："+str(sqlist.listLength()))
+    if sqlist.listEmpty()==True:
+        print("链表为空")
+    else:
+        print("链表不为空")
+    sqlist.printSpe()
+    sqlist.printPos()
+    n3 = int(input("请输入要插入的数据：\n"))
+    node3=Node(n3)
+    sqlist.insertSpe(node3)
     sqlist.print()
-    sqlist.deletespe()
+    sqlist.deleteSpe()
     sqlist.print()
     sqlist.delete()
     sqlist.print()
